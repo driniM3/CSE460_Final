44d48c9c5c3a44f60b79846825464ddeb84853cd
Atothendrew <atothendrew24@mac.com>
2014-04-18 16:13:25 -0700

    Requirements doc 5, 10 and 11 are now done
    This commit:
    
    - Allows Users (workers) to be able to see their assigned requirements
    and edit the status of them.
    - Allows tenants to view all the projects
    - Allows tenants to view all the requirements.

--- ProjectManagement/Controllers/Views/UserController.cs
+++ ProjectManagement/Controllers/Views/UserController.cs
@@ -19,9 +19,25 @@ namespace ProjectManagement.Controllers.Views
         {
             if (db.Personnels.Where(x => x.Id == User.Identity.Name).Single().Tenant.Name.ToLower() == tenant.ToLower())
             {
-                return View(db.Tenants.Where(x => x.Name == tenant).Single());
+                Tenant ten = db.Tenants.Where(x => x.Name == tenant).Single();
+                ViewBag.Username = User.Identity.Name;
+                ViewBag.UserProjects = db.Projects.Where(x => x.ProjectPersonnels.Any(m => m.PersonnelId == User.Identity.Name)).ToList();
+                ViewBag.UserRequirements = db.Requirements.Where(x => x.Personnel.Name == User.Identity.Name).ToList();
+                return View(ten);
             }
             return RedirectToAction("NotFound", "Error");
+        }
+
+        public ActionResult EditRequirementStatus(String requirementId, String newStatus)
+        {
+            int requirementIdInt = Convert.ToInt32(requirementId);
+            Requirement requirement = db.Requirements.Where(x => x.Id == requirementIdInt).Single();
+
+            requirement.Status = newStatus;
+            db.Entry(requirement).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", new { });
         }
 
     }
